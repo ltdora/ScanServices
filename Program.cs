33df1f2fd4ac951384ef403943b2090390eba8f2
ltdora <162949620+ltdora@users.noreply.github.com>
2024-05-21 23:00:11 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Quartz.Impl;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+using ScanServices.Scan;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ScanServices
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new MongoClient("mongodb+srv://datlt:Laitiendat1312.@helloworld.bbqg5uv.mongodb.net/?retryWrites=true&w=majority&appName=HelloWorld");
+            var database = client.GetDatabase("HelloMongo");
+            var collection = database.GetCollection<BsonDocument>("Users");
+
+            // Cấu hình Quartz.NET
+            IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+            await scheduler.Start();
+
+            // Định nghĩa công việc và liên kết với lớp FolderScanJob
+            IJobDetail job = JobBuilder.Create<FolderScanJob>()
+                .WithIdentity("folderScanJob", "group1")
+                .Build();
+
+            // Tạo lịch trình để chạy công việc
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(3) // Chạy mỗi 3 giây
+                    .RepeatForever())
+                .Build();
+
+            // Liên kết công việc và lịch trình với Scheduler
+            await scheduler.ScheduleJob(job, trigger);
+
+            // Giữ ứng dụng console chạy
+            Console.WriteLine("Press [Enter] to close the application.");
+            Console.ReadLine();
+
+            await scheduler.Shutdown();
+        }
+    }
+}
---
+++ Services/FolderScanJob.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ScanServices.Scan
+{
+    public class FolderScanJob : IJob
+    {
+        private static HashSet<string> processedFiles = new HashSet<string>();
+        private readonly string folderPath = "C:\\Users\\Admin\\Desktop\\QuantzScan";
+        private readonly string processedFolderPath = "C:\\Users\\Admin\\Desktop\\QuantzScan\\readed";
+
+        private readonly IMongoCollection<BsonDocument> _collection;
+
+        public FolderScanJob()
+        {
+            var client = new MongoClient("mongodb+srv://datlt:Laitiendat1312.@helloworld.bbqg5uv.mongodb.net/?retryWrites=true&w=majority&appName=HelloWorld");
+            var database = client.GetDatabase("HelloMongo");
+            _collection = database.GetCollection<BsonDocument>("Users");
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                // Tạo thư mục đích nếu chưa tồn tại
+                if (!Directory.Exists(processedFolderPath))
+                {
+                    Directory.CreateDirectory(processedFolderPath);
+                }
+
+                var files = Directory.GetFiles(folderPath);
+                foreach (var file in files)
+                {
+                    if (!processedFiles.Contains(file))
+                    {
+                        var fileName = Path.GetFileName(file);
+                        Console.WriteLine($"Read file: {fileName}");
+
+                        // Đọc nội dung file
+                        string[] content = File.ReadAllLines(file);
+                        string field = content[0];
+                        string value = content[1];
+
+                        var document = new BsonDocument { { field, value } };
+                        Console.WriteLine(document);
+
+                        //_collection.InsertOneAsync(document);
+
+                        var getall = _collection.Find(_ => true).ToList();
+                        foreach (var item in getall)
+                        {
+                            Console.WriteLine(item);
+                        }
+
+                        Console.WriteLine($"Saved {fileName} to Database");
+
+                        // Di chuyển file sau khi đã xử lý xong
+                        var destFile = Path.Combine(processedFolderPath, fileName);
+
+                        try
+                        {
+                            File.Move(file, destFile);
+                            Console.WriteLine($"File moved to: {destFile}");
+
+                            // Thêm file vào danh sách đã xử lý
+                            processedFiles.Add(destFile);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error moving file {fileName}: {ex.Message}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"File already processed: {file}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Folder {folderPath} does not exist.");
+            }
+            return Task.CompletedTask;
+        }
+
+    }
+}
